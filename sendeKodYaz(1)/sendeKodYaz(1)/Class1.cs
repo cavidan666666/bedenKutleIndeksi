6b737f0578352d4cfe0e92391510a40b39f27d05
Cavidan <acavidan.p102@code.edu.az>
2017-09-02 17:20:36 +0400

    final commit

---
+++ sendeKodYaz(1)/sendeKodYaz(1)/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sendeKodYaz_1_
+{
+    class Body
+    {
+        public double boy;
+        public double kilo;
+        public double bedenKutleIndeksi;
+
+        public Body(double _boy,double _kilo,double _bedenKutleIndeksi)
+        {
+            this.boy = _boy;
+            this.kilo = _boy;
+            this.bedenKutleIndeksi = _bedenKutleIndeksi;
+                
+      }
+      
+    }
+}
---
+++ sendeKodYaz(1)/sendeKodYaz(1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sendeKodYaz_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Boyunuzu Yazin");
+            Double uzunluq =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Cekinizi Yazin");
+            Double ceki =Convert.ToDouble(Console.ReadLine());
+            double indeks = ceki / (uzunluq * uzunluq);
+            Console.WriteLine("sizin indeksiniz {0}",indeks);
+            Body beden = new Body(uzunluq,ceki,indeks);
+            Console.ReadKey();
+        }
+    }
+    
+}
